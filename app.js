4fd9420d29e163baa57a890700325c73124294a4
Hrishu Devulapalli <hrishudev@gmail.com>
2024-06-06 09:09:39 -0700

    done

--- app.js
+++ app.js
@@ -4,7 +4,27 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = null;
+let lastName = null;
+var age = null;
 // Exercise 2
-
+firstName = "Hrishu";
+lastName = "Devulapalli"
+age = 18;
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStack = false;
+let slectedSize = "M";
+// Exercise 5
+let title = "Name of the Wind"
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
